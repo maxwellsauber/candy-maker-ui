6e9dcbdde4554f7bdbba1b542ea81c823cb37817
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-05-27 22:27:13 -0400

    candy time

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+const { getAllManufacturers, getManufacturerById } = require('./controllers/manufacturers')
+const { getAllProducts, getProductsById } = require('./controllers/products')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/api/manufacturers', getAllManufacturers)
+
+app.get('/api/manufacturers/:id', getManufacturerById)
+
+app.get('/api/products', getAllProducts)
+
+app.get('/api/products/:id', getProductsById)
+
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+
+app.listen(1337, () => {
+  console.log('Listening on port 1337...') // eslint-disable-line no-console
+})
